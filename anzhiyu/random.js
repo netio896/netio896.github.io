9e0bb8b76cc0dd0603b5160d8e5c426783afd949
netio896 <nainghtweyan419@gmail.com>
2025-01-07 03:17:08 +0630

    Site updated: 2025-01-07 03:17:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/07/hello-world/"];function toRandomPost(){
+var posts=["2025/01/07/搭建属于自己的发卡小店-独角数卡/","2025/01/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
