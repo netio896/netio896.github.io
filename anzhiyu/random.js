437a9ce8ea0a00ad0f97959f392081a9a9cd6504
netio896 <nainghtweyan419@gmail.com>
2025-01-12 23:03:10 +0630

    Site updated: 2025-01-12 23:03:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/07/Docker搭建自己的专属订阅链接转换服务/","2025/01/07/OpenWRT-PVE-LXC-CT容器下的安装/","2025/01/07/搭建属于自己的发卡小店-独角数卡/","2025/01/07/hello-world/"];function toRandomPost(){
+var posts=["2025/01/07/Docker搭建自己的专属订阅链接转换服务/","2025/01/07/OpenWRT-PVE-LXC-CT容器下的安装/","2025/01/07/istoreos无线网卡驱动/","2025/01/07/LskyPro图床/","2025/01/07/搭建属于自己的发卡小店-独角数卡/","2025/01/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
