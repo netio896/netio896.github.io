f757daf2e1be86bce9aa27bc09eb9c337d459332
netio896 <nainghtweyan419@gmail.com>
2025-11-27 20:43:11 +0630

    Site updated: 2025-11-27 20:43:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/26/hello-world/","2025/11/26/pve9-upgrade-kit-1/","2025/11/26/pve9-upgrade-kit/"];function toRandomPost(){
+var posts=["2025/11/26/pve9-upgrade-kit-1/","2025/11/26/pve9-upgrade-kit/","2025/11/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
